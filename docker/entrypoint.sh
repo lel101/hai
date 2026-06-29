577cf2b3c5e5c95932e9a744a7de9342cca0ec9e
lel101 <baidianjhon@gmail.com>
2026-06-29 23:12:04 +0800

    Merge branch 'main' of github-lel101:lel101/hai

---
+++ docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+cd /var/www/html
+
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan migrate --force
+
+exec /start.sh
